f516e650e08e87f68359c7d56db098eb6658366b
Fernando Soares <dotfernando@gmail.com>
2017-08-24 22:13:37 -0300

    subindo

---
+++ template-islands.js
@@ -0,0 +1,27 @@
+const container = document.querySelector('.container');
+const bg = document.querySelector('.background');
+const el = document.querySelectorAll('.island');
+const walk = 30;
+
+function moveEl(e) {
+    const { offsetWidth:width, offsetHeight: height} = container;
+    let { offsetX: x, offsetY: y} = e;
+   
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * walk) - (walk/2));
+    const yWalk = Math.round((y / height * walk) - (walk/2));
+
+    // Adiciona o efeito nas islands
+    for (let item of el) {
+        item.style.transform = `translate(${-xWalk}px, ${-yWalk}px)`
+    }
+
+    // Adiciona o efeito no background
+    bg.style.transform = `translate(${xWalk / 2}px, ${yWalk / 2}px) scale(2)`
+}
+
+container.addEventListener('mousemove', moveEl);
